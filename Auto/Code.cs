b1a40bf2e1a7dd19979435a7852678218e274c1c
Mixa1997 <43315299+Mixa1997@users.noreply.github.com>
2021-02-16 13:25:11 +0300

    Add files via upload

---
+++ Auto/Code.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto
+{
+    class Code
+    {
+        public int OneNamber { get; set; }
+        public int TwoNamber { get; set; }
+        public Code(int onenumber, int twonamber)
+        {
+            OneNamber = onenumber;
+            TwoNamber = twonamber;
+        }
+        public void Result()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            if (OneNamber <= TwoNamber) Console.WriteLine("Входит в диапозон");
+            if (TwoNamber >= OneNamber) Console.WriteLine("Входит в диапозон");
+            else Console.WriteLine("Не входит в диапозон");
+        }
+    }
+}
---
+++ Auto/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write($"Введите первое число:");
+                int onenamber = int.Parse(Console.ReadLine());
+                Console.Write($"Введите второе число:");
+                int twonamber = int.Parse(Console.ReadLine());
+                Code code = new Code(onenamber, twonamber);
+                code.Result();
+            }
+            catch(Exception er)
+            {
+                Console.WriteLine(er.Message);
+            }
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
